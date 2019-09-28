fb9b6646c4ecf6514af7a838c92891a0232adf64
Tahj Chisolm <tahjchisolm21@gmail.com>
2019-09-28 04:39:52 -0400

    Update dbh.php
    update to resolve access issues, as well as image inclusion

--- dbh.php
+++ dbh.php
@@ -1,10 +1,22 @@
 <?php
 
-$servername = "localhost";
+$servername = "127.0.0.1";
 $username = "frodo";
 $password = "";
 $dbname = "nemours";
 
 $conn = mysqli_connect($servername, $username, $password, $dbname);
+
+if (mysqli_connect_errno()) {
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+// or die("Unable to connect to MySQL");
+else {
+    echo "Connected to MySQL<br>";
+}
+
+$mysqli = new mysqli($servername, $username, $password, $dbname) or die($mysqli->connect_error);
+$table = 'image';
+//$conn = mysql_connect("localhost","root","");
 
 ?>
