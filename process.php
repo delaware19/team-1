7f40e0bfcb6aa27aec6fe1bee9b3879ca04028c3
Victoria Schimansky <vschimansky@yahoo.com>
2019-09-28 07:17:00 -0400

    the new and improved process

--- process.php
+++ process.php
@@ -32,7 +32,7 @@ if(isset($_POST['dropbtn'])){
 }
 
 echo "Insert into nemours, $tablename($gender, $agebracket, $ethnicity, $firstname)";
-$sql = "Insert into nemours, $tablename($gender, $agebracket, $ethnicity, $firstname)";
+$sql = "Insert into $tablename($gender, $agebracket, $ethnicity, $firstname)";
 $result = mysqli_query($conn, $sql);
 echo "finished";
 ?>
